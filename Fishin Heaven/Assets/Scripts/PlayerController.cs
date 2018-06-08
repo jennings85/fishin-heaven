2914abd18fdb7b5cd4532889dc336a36970f4eb6
Unknown <jenningsrandolph@gmail.com>
2018-06-08 09:04:33 -0500

    removed stamina to focus on camera
    rework stamina at a later date

--- Fishin Heaven/Assets/Scripts/PlayerController.cs
+++ Fishin Heaven/Assets/Scripts/PlayerController.cs
@@ -8,47 +8,32 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField]
 	private float speed;
 	[SerializeField]
-	private int energy = 100;
-	[SerializeField]
 	private float runMult;
 	[SerializeField]
 	private float jumpPower;
 
 	private float x;
 	private float z;
-	private float groundDist;
+	private bool grounded;
 	private Rigidbody RB;
 	private Collider collider;
-	private float distToGround;
-	public Text energyText;
-	public Image energyPic;
 
 	void Start () {
 		RB = gameObject.GetComponent<Rigidbody> ();
-		collider = gameObject.GetComponent<CapsuleCollider> ();
-		distToGround = collider.bounds.extents.y;
 	}
 
 	void Update () {
-		energyText.text = "Energy: " + energy;
-		energyPic.rectTransform.sizeDelta = new Vector2 (energy * 3, 50);
-		x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f * speed;
+		grounded = (Physics.Raycast (transform.position, -Vector3.up, + 0.18f));
+		x = Input.GetAxis ("Horizontal") * Time.deltaTime * 100.0f * speed;
 		z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f * speed;
-		if (Input.GetKey("left shift") && energy > 3 && z > 0) {
+		if (Input.GetKey("left shift") && z > 0 && grounded) {
 			z *= runMult;
-			energy -= 3;
 		}
 		RB.velocity *= .99f;
 		transform.Rotate (0, x, 0);
 		transform.Translate (0, 0, z);
-		if (Input.GetKeyDown(KeyCode.Space) && (Physics.Raycast(transform.position, -Vector3.up, distToGround + 0f)) && energy == 100){
+		if (Input.GetKeyDown(KeyCode.Space) && grounded) {
 			RB.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
-			energy = 0;
-		}
-		if (energy == 99) {
-			energy = 100;
-		} else if (energy < 99) {
-			energy += 2;
 		}
 	}
 }
